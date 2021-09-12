7c8833e75b393e194208b2c39047d33b6cf17801
Philip Arturo Smith <smithy545@gmail.com>
2021-09-12 03:14:45 -0500

    Basic typescript

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-let http = require('http');
-let fs = require('fs');
-let path = require('path');
-
-http.createServer(function(req, res) {
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+http.createServer(function (req, res) {
     console.log("Request hit");
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end("Test response", 'utf-8');
 }).listen(80);
 console.log("Server started");
