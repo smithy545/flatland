07647e1e59b78aef70a0178ede8407f483cb171d
Philip Arturo Smith <smithy545@gmail.com>
2021-09-12 16:20:33 -0500

    Setup webgl canvas

--- shared/game.js
+++ shared/game.js
@@ -1,4 +1,22 @@
 console.log("test game");
+
+
+function main() {
+    const canvas = document.querySelector("#gameCanvas");
+    const gl = canvas.getContext("webgl");
+
+    if(gl === null) {
+        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+window.onload = main;
+
+
 let socket = new WebSocket("ws://192.168.0.29:99");
 
 socket.onopen = function(e) {
@@ -14,10 +32,10 @@ socket.onclose = function(evt) {
     if(evt.wasClean) {
         console.log(`Clean close, code=${evt.code} reason=${evt.reason}`)
     } else {
-        console.log("Connection died");
+        console.error("Connection died");
     }
 };
 
 socket.onerror = function(err) {
-    console.error(err);
+    console.error(`WebSocket error: ${err}`);
 };
