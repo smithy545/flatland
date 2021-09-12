1bf7398961fad2afcc112d585a738ccac79bec78
Philip Smith <smithy545@gmail.com>
2021-09-12 04:23:30 -0500

    Update node server

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,8 +1,27 @@
-var http = require("http");
-var fs = require("fs");
-var server = http.createServer(function (req, res) {
-    console.log("hit");
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.createReadStream('../shared/index.html').pipe(res);
-}).listen(80);
+import fs from "fs";
+import http from "http";
+import path from "path";
+
+const staticBasePath = '../shared';
+
+const server = http.createServer(function (req, res) {
+    const resolvedBase = path.resolve(staticBasePath);
+    const safeSuffix = path.normalize(req.url).replace(/^(\.\.[\/\\])+/, '');
+    const fileLoc = path.join(resolvedBase, safeSuffix);
+
+    fs.readFile(fileLoc, function(err, data) {
+        if(err) {
+            res.writeHead(404, "Not found");
+            res.write("404: Could not find what you're looking for");
+            return res.end();
+        }
+
+        res.statusCode = 200;
+        res.write(data);
+        return res.end();
+    })
+});
+const port = 80;
+server.listen(80);
+
 console.log("Server started");
