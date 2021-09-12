378498495b7de08fa49d77e2a6eb8c96781a1942
Philip Arturo Smith <smithy545@gmail.com>
2021-09-12 03:28:38 -0500

    make simple c++ server

--- index.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-http.createServer(function (req, res) {
-    console.log("Request hit");
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end("Test response", 'utf-8');
-}).listen(80);
-console.log("Server started");
